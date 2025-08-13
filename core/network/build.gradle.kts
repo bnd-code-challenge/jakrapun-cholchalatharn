ef3c7dd11e7b3a5f2dc644803c5eb8b27efb2c38
Sarawut.Kengkan <brandnewday.codechallenge@outlook.com>
2025-08-13 15:29:52 +0700

    chore: init projet

---
+++ core/network/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.ksp)
+
+    id("kotlin-parcelize")
+}
+
+android {
+    namespace = "com.chuu.network"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    //retrofit
+    api(libs.retrofit2)
+    api(libs.retrofit2.gsonConverter)
+    api(libs.retrofit2.okhttp)
+    api(libs.retrofit2.okhttpLoggingInterceptor)
+
+    api(libs.google.code.gson)
+
+    //coroutines
+    implementation(libs.kotlin.coroutines)
+
+    //koin
+    api(project.dependencies.platform(libs.koin.bom))
+    api(libs.koin.core)
+    api(libs.koin.android)
+
+    //room
+    api(libs.room)
+    api(libs.room.ktx)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
